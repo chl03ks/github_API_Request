7e4926d5355d294ff58515d043a03a35bbd050ff
Dan <udevelopd@gmail.com>
2015-08-12 17:11:05 -0500

    adding form to the app for searching username on github

--- githubApp/views.py
+++ githubApp/views.py
@@ -13,21 +13,21 @@ def test(request):
 
 
 def profile(request):
-    jsonList = []
-    req = requests.get('https://api.github.com/users/gvsdan')
-
-    jsonList.append(json.loads(req.content))
     parsedData = []
-    userData = {}
-    for data in jsonList:
-        userData['name'] = data['name']
-        userData['blog'] = data['blog']
-        userData['email'] = data['email']
-        userData['public_gists'] = data['public_gists']
-        userData['public_repos'] = data['public_repos']
-        userData['avatar_url'] = data['avatar_url']
-        userData['followers'] = data['followers']
-        userData['following'] = data['following']
-
-    parsedData.append(userData)
+    if request.method == 'POST':
+        username = request.POST.get('user')
+        req = requests.get('https://api.github.com/users/' + username)
+        jsonList = []
+        jsonList.append(json.loads(req.content))
+        userData = {}
+        for data in jsonList:
+            userData['name'] = data['name']
+            userData['blog'] = data['blog']
+            userData['email'] = data['email']
+            userData['public_gists'] = data['public_gists']
+            userData['public_repos'] = data['public_repos']
+            userData['avatar_url'] = data['avatar_url']
+            userData['followers'] = data['followers']
+            userData['following'] = data['following']
+        parsedData.append(userData)
     return render(request, 'githubApp/profile.html', {'data': parsedData})
